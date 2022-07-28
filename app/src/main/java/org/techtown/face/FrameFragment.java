1be888ca16be5ddb3136c8dce9b0c1a8b71be4e0
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-07-28 20:25:19 +0900

    Make FrameFragment

---
+++ app/src/main/java/org/techtown/face/FrameFragment.java
@@ -0,0 +1,17 @@
+package org.techtown.face;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FrameFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_frame, container, false);
+        
+        return v;
+    }
+}
