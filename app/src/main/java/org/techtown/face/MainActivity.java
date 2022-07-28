8b4845931c74081d56af3afc43202c3e82ec98ba
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-07-28 16:47:52 +0900

    Add icon to actionBar

--- app/src/main/java/org/techtown/face/MainActivity.java
+++ app/src/main/java/org/techtown/face/MainActivity.java
@@ -4,11 +4,41 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //액션바를 위한 메서드
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        super.addContentView(view, params);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int curId = item.getItemId();
+        switch (curId) {
+            case R.id.add_family:
+                break;
+            case R.id.setting:
+                break;
+            case R.id.add_moment:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
