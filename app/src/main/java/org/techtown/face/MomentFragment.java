3140a67c8a61eb833371de151fbcd115f284faa7
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-07-28 20:29:32 +0900

    Make MomentFragment

---
+++ app/src/main/java/org/techtown/face/MomentFragment.java
@@ -0,0 +1,17 @@
+package org.techtown.face;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MomentFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_moment, container, false);
+
+        return v;
+    }
+}
