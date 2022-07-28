79567177df377cb2bfd9bb34d6d76e5e7c7fc137
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-07-28 16:27:57 +0900

    Make bottomNavigationView

--- app/src/main/java/org/techtown/face/MainActivity.java
+++ app/src/main/java/org/techtown/face/MainActivity.java
@@ -1,8 +1,12 @@
 package org.techtown.face;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.frameTab:
+
+                                return true;
+                            case R.id.scaleTab:
+
+                                return true;
+                            case R.id.momentTab:
+
+                                return true;
+                        }
+                        return false;
+                    }
+                }
+        );
     }
 }
