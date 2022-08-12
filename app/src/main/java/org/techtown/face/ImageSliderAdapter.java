bf94bd727119e597bd4b321ff4671e877f8b9c07
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-08-12 17:27:12 +0900

    Create ImageSliderAdapter class

---
+++ app/src/main/java/org/techtown/face/ImageSliderAdapter.java
@@ -0,0 +1,54 @@
+package org.techtown.face;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+public class ImageSliderAdapter extends RecyclerView.Adapter<ImageSliderAdapter.MyViewHolder> {
+    private Context context;
+    private String[] sliderImage;
+
+    public ImageSliderAdapter(Context context, String[] sliderImage) {
+        this.context = context;
+        this.sliderImage = sliderImage;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_slider, viewGroup, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.bindSliderImage(sliderImage[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return sliderImage.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imgSlider);
+        }
+
+        public void bindSliderImage(String imageURL) {
+            Glide.with(context)
+                    .load(imageURL)
+                    .into(imageView);
+        }
+    }
+}
