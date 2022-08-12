00a0570f78c1eaf23921066043ef5f606ac0b306
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-08-12 20:47:34 +0900

    Add indicator and image to myViewPager

--- app/src/main/java/org/techtown/face/MomentFragment.java
+++ app/src/main/java/org/techtown/face/MomentFragment.java
@@ -4,14 +4,85 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class MomentFragment extends Fragment {
+
+    public ViewPager2 myImageViewPager;
+    public LinearLayout layoutIndicator;
+
+    private String[] images = new String[] {
+            "https://cdn.pixabay.com/photo/2019/12/26/10/44/horse-4720178_1280.jpg",
+            "https://cdn.pixabay.com/photo/2020/11/04/15/29/coffee-beans-5712780_1280.jpg",
+            "https://cdn.pixabay.com/photo/2020/03/08/21/41/landscape-4913841_1280.jpg",
+            "https://cdn.pixabay.com/photo/2020/09/02/18/03/girl-5539094_1280.jpg",
+            "https://cdn.pixabay.com/photo/2014/03/03/16/15/mosque-279015_1280.jpg"};
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_moment, container, false);
 
+        //나의 순간 ViewPager2 다루기
+        myImageViewPager = v.findViewById(R.id.imgSlider);
+        layoutIndicator = v.findViewById(R.id.myImgIndicator);
+
+        myImageViewPager.setOffscreenPageLimit(1);
+        myImageViewPager.setAdapter(new ImageSliderAdapter(v.getContext(), images));
+
+        myImageViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentIndicator(position);
+            }
+        });
+
+        setupIndicator(images.length);
+        //
+
         return v;
+    }
+
+    //Indicator 설정
+    public void setupIndicator(int count) {
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+
+        params.setMargins(16, 8, 16, 8);
+
+        for (int i = 0; i<indicators.length; i++) {
+            indicators[i] =new ImageView(getContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(getContext(),
+                    R.drawable.bg_indicator_inactive));
+            indicators[i].setLayoutParams(params);
+            layoutIndicator.addView(indicators[i]);
+        }
+        setCurrentIndicator(0);
+    }
+
+    //현재 Indicator의 상태 설정
+    public void setCurrentIndicator(int position) {
+        int childCount = layoutIndicator.getChildCount();
+        for (int i = 0; i<childCount; i++) {
+            ImageView imageView = (ImageView) layoutIndicator.getChildAt(i);
+            if (i==position) {
+                imageView.setImageDrawable(ContextCompat.getDrawable(
+                        getContext(),
+                        R.drawable.bg_indicator_active
+                ));
+            } else {
+                imageView.setImageDrawable(ContextCompat.getDrawable(
+                        getContext(),
+                        R.drawable.bg_indicator_inactive
+                ));
+            }
+        }
     }
 }
