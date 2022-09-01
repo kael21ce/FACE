08c490f88de6b972cbd6e5fe376078234b086e7f
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-09-01 17:30:43 +0900

    Load name from FACEdatabase to set item for MomentFragment

--- app/src/main/java/org/techtown/face/MomentFragment.java
+++ app/src/main/java/org/techtown/face/MomentFragment.java
@@ -1,19 +1,30 @@
 package org.techtown.face;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import me.relex.circleindicator.CircleIndicator3;
 
 public class MomentFragment extends Fragment {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String TAG = "FACEdatabase";
 
     private ViewPager2 myImageViewPager;
     private CircleIndicator3 myImgIndicator;
@@ -60,10 +71,29 @@ public class MomentFragment extends Fragment {
         MomentAdapter momentAdapter = new MomentAdapter();
 
         momentAdapter.addItem(new Moment("나", images, dates));
-        momentAdapter.addItem(new Moment("홍길동", himages, hdates));
-        momentAdapter.addItem(new Moment("임꺽정", iimages, idates));
 
-        momentRecyclerView.setAdapter(momentAdapter);
+        //db에서 데이터 가져오기
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String name = document.get("name").toString();
+                                if (name.equals("홍길동")) {
+                                    momentAdapter.addItem(new Moment(name, himages, hdates));
+                                } else {
+                                    momentAdapter.addItem(new Moment(name, iimages, idates));
+                                }
+                                Log.w(TAG, "Successfully loaded");
+                            }
+                            momentRecyclerView.setAdapter(momentAdapter);
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
         //
 
 
