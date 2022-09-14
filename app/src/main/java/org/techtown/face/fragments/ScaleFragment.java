857915733cb05c7911147b14a5bede422cbf528b
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-09-14 15:39:02 +0900

    Load data for ScaleFragment except user's

--- app/src/main/java/org/techtown/face/fragments/ScaleFragment.java
+++ app/src/main/java/org/techtown/face/fragments/ScaleFragment.java
@@ -17,18 +17,22 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.techtown.face.utilites.Constants;
 import org.techtown.face.utilites.Family.FamilyScale;
 import org.techtown.face.R;
 import org.techtown.face.adapters.ScaleAdapter;
+import org.techtown.face.utilites.PreferenceManager;
 
 public class ScaleFragment extends Fragment {
 
+    PreferenceManager preferenceManager;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     String TAG = "FACEdatabase";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_scale, container, false);
+        preferenceManager = new PreferenceManager(getActivity().getApplicationContext());
 
         //리사이클러뷰 객체
         RecyclerView recyclerView = v.findViewById(R.id.scaleRecyclerView);
@@ -46,15 +50,18 @@ public class ScaleFragment extends Fragment {
         db.collection("users")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                String name = document.get("name").toString();
-                                String phone_number = document.get("phone_number").toString();
-                                float angle = Float.parseFloat(document.get("angle").toString());
-                                adapter.addItem(new FamilyScale(name, phone_number, angle));
-                                Log.w(TAG, "Successfully loaded");
+                                if (!currentUserId.equals(document.getId())) {
+                                    String name = document.get("name").toString();
+                                    String phone_number = document.get("phone_number").toString();
+                                    float angle = Float.parseFloat(document.get("angle").toString());
+                                    adapter.addItem(new FamilyScale(name, phone_number, angle));
+                                    Log.w(TAG, "Successfully loaded");
+                                }
                             }
                             recyclerView.setAdapter(adapter);
                         } else {
