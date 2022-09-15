17516656ee01e53b4aee24c662801c55d000bb8a
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-09-15 14:58:58 +0900

    Load FamilyActivity when interactButton is clicked

--- app/src/main/java/org/techtown/face/adapters/ScaleAdapter.java
+++ app/src/main/java/org/techtown/face/adapters/ScaleAdapter.java
@@ -2,6 +2,9 @@ package org.techtown.face.adapters;
 
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +14,23 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import org.techtown.face.activities.FamilyActivity;
+import org.techtown.face.models.User;
+import org.techtown.face.utilites.Constants;
 import org.techtown.face.utilites.Family;
 import org.techtown.face.R;
+import org.techtown.face.utilites.PreferenceManager;
 import org.techtown.face.utilites.ScaleInfo;
 
 import java.util.ArrayList;
 
 public class ScaleAdapter extends RecyclerView.Adapter<ScaleAdapter.ViewHolder>{
     ArrayList<Family.FamilyScale> items = new ArrayList<Family.FamilyScale>();
+    ArrayList<Family> itemsForButton = new ArrayList<Family>();
 
     @NonNull
     @Override
@@ -58,10 +70,16 @@ public class ScaleAdapter extends RecyclerView.Adapter<ScaleAdapter.ViewHolder>{
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+
+        PreferenceManager preferenceManager;
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String TAG = "FACEdatabase";
+
         TextView nameTxt;
         TextView incomingNum;
         TextView outgoingNum;
         ImageView scaleHead;
+        FloatingActionButton interactButton;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -70,6 +88,7 @@ public class ScaleAdapter extends RecyclerView.Adapter<ScaleAdapter.ViewHolder>{
             incomingNum = itemView.findViewById(R.id.incomingNum);
             outgoingNum = itemView.findViewById(R.id.outgoingNum);
             scaleHead = itemView.findViewById(R.id.scale_head);
+            interactButton = itemView.findViewById(R.id.interactButton);
 
         }
 
@@ -78,13 +97,50 @@ public class ScaleAdapter extends RecyclerView.Adapter<ScaleAdapter.ViewHolder>{
             ScaleInfo scaleInfo = new ScaleInfo();
 
             nameTxt.setText(item.getScaleName());
-            String mobile = item.getScaleMoible();
+            String mobileForScale = item.getScaleMoible();
             incomingNum.setText("수신: " + Integer.toString(scaleInfo.getIncomingNum(itemView.getContext(),
-                    mobile)));
+                    mobileForScale)));
             outgoingNum.setText("발신: " + Integer.toString(scaleInfo.getOutgoingNum(itemView.getContext(),
-                    mobile)));
-            float angle = scaleInfo.getAngle(itemView.getContext(), mobile);
+                    mobileForScale)));
+            float angle = scaleInfo.getAngle(itemView.getContext(), mobileForScale);
             scaleHead.setRotation(angle);
+
+            //interactButton 클릭 시 FamilyActivity로 넘어가기
+            interactButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    User user = new User();
+                    //db에서 데이터 가져오기
+                    db.collection(Constants.KEY_COLLECTION_USERS)
+                            .get()
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        if (document.get(Constants.KEY_MOBILE).equals(mobileForScale)) {
+                                            user.name = document.get(Constants.KEY_NAME).toString();
+                                            user.email = document.get(Constants.KEY_EMAIL).toString();
+                                            user.image= document.get(Constants.KEY_IMAGE).toString();
+                                            user.token = document.get(Constants.KEY_FCM_TOKEN).toString();
+                                            user.id = document.getId();
+                                            user.mobile = document.get(Constants.KEY_MOBILE).toString();
+                                            user.min_contact = Integer.parseInt(document.get(Constants.KEY_MIN_CONTACT).toString());
+                                            user.ideal_contact = Integer.parseInt(document.get(Constants.KEY_IDEAL_CONTACT).toString());
+                                            user.like = document.get(Constants.KEY_THEME_LIKE).toString();
+                                            user.dislike = document.get(Constants.KEY_THEME_DISLIKE).toString();
+                                            //Intent 전송
+                                            Intent contactIntent = new Intent(v.getContext(), FamilyActivity.class);
+                                            contactIntent.putExtra(Constants.KEY_USER, user);
+                                            v.getContext().startActivity(contactIntent);
+                                            Log.w(TAG, "Successfully loaded from ScaleAdapter");
+                                        }
+                                    }
+                                } else {
+                                    Log.w(TAG, "Error getting documents.", task.getException());
+                                }
+                            });
+
+                }
+            });
 
 
         }
