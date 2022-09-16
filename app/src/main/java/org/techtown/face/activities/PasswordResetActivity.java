ac63d29e449c23203047ff68028d9a9fc0e729ff
강지원 <kjason08@kaist.ac.kr>
2022-09-16 15:41:28 +0900

    add mamifast PasswordResetActivity, change activity_account

--- app/src/main/java/org/techtown/face/activities/PasswordResetActivity.java
+++ app/src/main/java/org/techtown/face/activities/PasswordResetActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -16,15 +17,19 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import org.techtown.face.databinding.ActivityPasswordResetBinding;
+import org.techtown.face.utilites.Constants;
+import org.techtown.face.utilites.PreferenceManager;
 
 public class PasswordResetActivity extends AppCompatActivity {
 
     private ActivityPasswordResetBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityPasswordResetBinding.inflate(getLayoutInflater());
+        preferenceManager = new PreferenceManager(getApplicationContext());
         setContentView(binding.getRoot());
         binding.textView.setOnClickListener(view -> {
             emailPassword();
@@ -34,12 +39,12 @@ public class PasswordResetActivity extends AppCompatActivity {
     }
 
     private void emailPassword(){
-        String emailAddress = "user@example.com";
+        String emailAddress = preferenceManager.getString(Constants.KEY_EMAIL);
         FirebaseAuth auth = FirebaseAuth.getInstance();
         auth.sendPasswordResetEmail(emailAddress)
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        Log.d(TAG, "Email sent.");
+                        showToast("Email sent.");
                     }
                 });
     }
@@ -54,5 +59,9 @@ public class PasswordResetActivity extends AppCompatActivity {
                         Log.d(TAG, "User password updated.");
                     }
                 });
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
