428bdfc1cedc331ba0adc02c9af70f4a032f4a24
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-09-23 23:09:06 +0900

    Update window in FACEdatabase when telephone rings

--- app/src/main/java/org/techtown/face/utilites/ContactReceiver.java
+++ app/src/main/java/org/techtown/face/utilites/ContactReceiver.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 public class ContactReceiver extends BroadcastReceiver {
 
     String RTAG = "ContactReceiver";
-
+    PreferenceManager preferenceManager;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     String TAG = "FACEdatabase";
 
@@ -27,16 +27,33 @@ public class ContactReceiver extends BroadcastReceiver {
         Log.i(RTAG, "onReceive() is called.");
         if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
             String callingMobile = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+            long now = System.currentTimeMillis();
             Log.i(TAG, "Calling mobile: " + callingMobile);
             db.collection(Constants.KEY_COLLECTION_USERS)
                     .get()
                     .addOnCompleteListener(task -> {
+                        String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 if (Objects.equals(document.get(Constants.KEY_MOBILE), callingMobile)) {
-                                    //데이터베이스에 expression 업데이트
+                                    //상대방 데이터베이스에 expression 업데이트
                                     db.collection(Constants.KEY_COLLECTION_USERS).document(document.getId())
                                                     .update("expression", 5);
+                                    //상대방 데이터베이스에 window 업데이트
+                                    db.collection(Constants.KEY_COLLECTION_USERS)
+                                            .document(document.getId())
+                                                    .collection(Constants.KEY_COLLECTION_USERS)
+                                                            .document(currentUserId)
+                                                                    .update("window", now);
+                                    //나의 데이터베이스에 expression 업데이트
+                                    db.collection(Constants.KEY_COLLECTION_USERS).document(currentUserId)
+                                            .update("expression", 5);
+                                    //나의 데이터베이스에 window 업데이트
+                                    db.collection(Constants.KEY_COLLECTION_USERS)
+                                            .document(currentUserId)
+                                            .collection(Constants.KEY_COLLECTION_USERS)
+                                            .document(document.getId())
+                                            .update("window", now);
                                     Log.i(TAG, "Update expression and window by " + RTAG);
                                 } else {
                                     Log.i(RTAG, "Not update " + TAG);
