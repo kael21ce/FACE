4171bf036900ae64027b469cdb4d05c8e9e119c4
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-09-23 17:04:21 +0900

    Create ContactReceiver class

---
+++ app/src/main/java/org/techtown/face/utilites/ContactReceiver.java
@@ -0,0 +1,15 @@
+package org.techtown.face.utilites;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ContactReceiver extends BroadcastReceiver {
+
+    String TAG = "ContactReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+    }
+}
