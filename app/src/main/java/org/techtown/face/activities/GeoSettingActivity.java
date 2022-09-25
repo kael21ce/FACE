6d3134595cdf8aa6d8bc52e027ac7dd1f879fa19
강지원 <kjason08@kaist.ac.kr>
2022-09-25 20:03:33 +0900

    set layout in activity_geo_setting

--- app/src/main/java/org/techtown/face/activities/GeoSettingActivity.java
+++ app/src/main/java/org/techtown/face/activities/GeoSettingActivity.java
@@ -16,6 +16,6 @@ public class GeoSettingActivity extends AppCompatActivity {
         binding = ActivityGeoSettingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
+        
     }
 }
