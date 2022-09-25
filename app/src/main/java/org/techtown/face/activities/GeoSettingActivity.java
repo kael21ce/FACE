2ed0e23321f2c6070e3f64ed7424c2daa5772622
강지원 <kjason08@kaist.ac.kr>
2022-09-25 19:51:05 +0900

    add GeoService, GeoSettingActivity, activity_geo_setting

---
+++ app/src/main/java/org/techtown/face/activities/GeoSettingActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.face.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeoSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo_setting);
+    }
+}
