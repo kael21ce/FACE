c4fce761d51a9d29feb66af7918916be05666618
강지원 <kjason08@kaist.ac.kr>
2022-09-25 13:01:42 +0900

    Complete MeetAdapter and MeetItem

--- app/src/main/java/org/techtown/face/adapters/MeetAdapter.java
+++ app/src/main/java/org/techtown/face/adapters/MeetAdapter.java
@@ -6,14 +6,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 import org.techtown.face.R;
 import org.techtown.face.models.MeetItem;
+import org.techtown.face.utilites.Constants;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MeetAdapter extends RecyclerView.Adapter<MeetAdapter.ViewHolder> {
     ArrayList<MeetItem> items = new ArrayList<>();
@@ -24,6 +33,7 @@ public class MeetAdapter extends RecyclerView.Adapter<MeetAdapter.ViewHolder> {
         TextView mobileTxt;
         Button delete;
         Button addMeet;
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -36,6 +46,43 @@ public class MeetAdapter extends RecyclerView.Adapter<MeetAdapter.ViewHolder> {
         }
 
         public void setItem(MeetItem item){
+            StorageReference imageRef = FirebaseStorage.getInstance().getReference().child(item.getPath());
+            imageRef.getDownloadUrl().addOnSuccessListener(downloadUrl -> Glide.with(itemView)
+                    .load(downloadUrl.toString())
+                    .into(imageView));
+            nameTxt.setText(item.getName());
+            mobileTxt.setText(item.getMobile());
+            delete.setOnClickListener (view -> {
+                delete.setVisibility(View.INVISIBLE);
+                addMeet.setVisibility(View.INVISIBLE);
+                Toast.makeText(itemView.getContext(), "삭제되었습니다",Toast.LENGTH_SHORT).show();
+            });
+
+            long now = System.currentTimeMillis();
+            HashMap<String, Object> meet = new HashMap<>();
+            meet.put(Constants.KEY_WINDOW, now);
+            meet.put(Constants.KEY_EXPRESSION, 5);
+
+            addMeet.setOnClickListener(v -> {
+                db.collection(Constants.KEY_COLLECTION_USERS)
+                        .document(item.getMyId())
+                        .collection(Constants.KEY_COLLECTION_USERS)
+                        .document(item.getUserId())
+                        .update(meet);
+                db.collection(Constants.KEY_COLLECTION_USERS)
+                        .document(item.getUserId())
+                        .collection(Constants.KEY_COLLECTION_USERS)
+                        .document(item.getMyId())
+                        .update(meet);
+                delete.setVisibility(View.INVISIBLE);
+                addMeet.setVisibility(View.INVISIBLE);
+                db.collection(Constants.KEY_COLLECTION_USERS)
+                        .document(item.getMyId())
+                        .collection(Constants.KEY_COLLECTION_NOTIFICATION)
+                        .document(item.getDocId())
+                        .delete()
+                        .addOnCompleteListener(task -> Toast.makeText(itemView.getContext(), "가족으로 추가되었습니다",Toast.LENGTH_SHORT).show());
+            });
 
         }
     }
--- app/src/main/java/org/techtown/face/models/MeetItem.java
+++ app/src/main/java/org/techtown/face/models/MeetItem.java
@@ -1,4 +1,69 @@
 package org.techtown.face.models;
 
 public class MeetItem {
+    String path;
+    String name;
+    String mobile;
+    String myId;
+    String userId;
+    String docId;
+
+    public MeetItem(String path, String name, String mobile, String myId, String userId, String docId){
+        this.path = path;
+        this.name = name;
+        this.mobile = mobile;
+        this.myId = myId;
+        this.userId = userId;
+        this.docId =docId;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMyId() {
+        return myId;
+    }
+
+    public void setMyId(String myId) {
+        this.myId = myId;
+    }
+
+    public String getDocId() {
+        return docId;
+    }
+
+    public void setDocId(String docId) {
+        this.docId = docId;
+    }
+
+
 }
