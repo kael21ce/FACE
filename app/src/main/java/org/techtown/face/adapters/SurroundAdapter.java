d0f23aa1042b897a30f999861a5c801e8bcfda72
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-09-28 18:18:44 +0900

    Create Bluetooth class

--- app/src/main/java/org/techtown/face/adapters/SurroundAdapter.java
+++ app/src/main/java/org/techtown/face/adapters/SurroundAdapter.java
@@ -3,19 +3,31 @@ package org.techtown.face.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.techtown.face.R;
+import org.techtown.face.models.Bluetooth;
 import org.techtown.face.models.Family;
+import org.techtown.face.models.User;
 
 import java.util.ArrayList;
 
 public class SurroundAdapter extends RecyclerView.Adapter<SurroundAdapter.ViewHolder> {
-    ArrayList items = new ArrayList();
-    ArrayList<Family> itemFamily = new ArrayList<>();
+    ArrayList<Bluetooth> items = new ArrayList<>();
+
+    public interface OnItemClickListener {
+        void onItemClicked(int position, User user);
+    }
+
+    private PairedAdapter.OnItemClickListener itemClickListener;
+
+    public void setOnItemClickListener (PairedAdapter.OnItemClickListener listener) {
+        itemClickListener=listener;
+    }
 
     @NonNull
     @Override
@@ -41,6 +53,7 @@ public class SurroundAdapter extends RecyclerView.Adapter<SurroundAdapter.ViewHo
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView pairedName;
         TextView statusText;
+        Button connectButton;
 
 
         public ViewHolder(View itemVIew) {
@@ -48,6 +61,7 @@ public class SurroundAdapter extends RecyclerView.Adapter<SurroundAdapter.ViewHo
 
             pairedName = itemVIew.findViewById(R.id.pairedName);
             statusText = itemVIew.findViewById(R.id.statusText);
+            connectButton = itemVIew.findViewById(R.id.connectButton);
         }
 
         public void setItem(Family item) {
---
+++ app/src/main/java/org/techtown/face/models/Bluetooth.java
@@ -0,0 +1,13 @@
+package org.techtown.face.models;
+
+public class Bluetooth {
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    String device;
+}
