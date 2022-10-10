bb477571a57a9fcb1ca23f738dccc682573b31c4
강지원 <kjason08@kaist.ac.kr>
2022-10-11 00:56:44 +0900

    Change FamilySettingActivity and FamilySettingAdapter

--- app/src/main/java/org/techtown/face/activities/FamilySettingActivity.java
+++ app/src/main/java/org/techtown/face/activities/FamilySettingActivity.java
@@ -1,20 +1,89 @@
 package org.techtown.face.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import org.techtown.face.R;
+import org.techtown.face.adapters.FamilySettingAdapter;
 import org.techtown.face.databinding.ActivityFamilyAddBinding;
+import org.techtown.face.databinding.ActivityFamilySettingBinding;
 import org.techtown.face.databinding.ActivitySettingBinding;
+import org.techtown.face.models.Family;
+import org.techtown.face.models.User;
+import org.techtown.face.utilites.Constants;
+import org.techtown.face.utilites.PreferenceManager;
 
 public class FamilySettingActivity extends AppCompatActivity {
-    ActivityFamilyAddBinding binding;
+    ActivityFamilySettingBinding binding;
+    PreferenceManager preferenceManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityFamilyAddBinding.inflate(getLayoutInflater());
+        binding = ActivityFamilySettingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.recyclerView.setLayoutManager(layoutManager);
+        FamilySettingAdapter adapter = new FamilySettingAdapter();
+        preferenceManager = new PreferenceManager(getApplicationContext());
 
+        String myId = preferenceManager.getString(Constants.KEY_USER_ID);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection(Constants.KEY_COLLECTION_USERS)
+                .document(myId)
+                .collection(Constants.KEY_COLLECTION_USERS)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            User user = new User();
+                            user.min_contact = Integer.parseInt(document.get(Constants.KEY_MIN_CONTACT).toString());
+                            user.ideal_contact = Integer.parseInt(document.get(Constants.KEY_IDEAL_CONTACT).toString());
+                            user.like = document.get(Constants.KEY_THEME_LIKE).toString();
+                            user.dislike = document.get(Constants.KEY_THEME_DISLIKE).toString();
+                            user.id = document.getString(Constants.KEY_USER);
+                            int expression = Integer.parseInt(document.get(Constants.KEY_EXPRESSION).toString());
+                            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+                            firestore.collection(Constants.KEY_COLLECTION_USERS).document(user.id).get().addOnCompleteListener(task1 -> {
+                                if(task1.isSuccessful()){
+                                    DocumentSnapshot snapshot = task1.getResult();
+                                    user.name = snapshot.get(Constants.KEY_NAME).toString();
+                                    user.image= snapshot.get(Constants.KEY_IMAGE).toString();
+                                    user.mobile = snapshot.get(Constants.KEY_MOBILE).toString();
+                                    user.path = snapshot.get(Constants.KEY_PATH).toString();
+
+
+                                    if (expression==5) {
+                                        adapter.addItem(new Family(user));
+                                    } else if (expression==4) {
+                                        adapter.addItem(new Family(user));
+                                    } else if (expression==3) {
+                                        adapter.addItem(new Family(user));
+                                    } else if (expression==2) {
+                                        adapter.addItem(new Family(user));
+                                    } else if (expression==1) {
+                                        adapter.addItem(new Family(user));
+                                    } else {
+                                        adapter.addItem(new Family(user));
+                                    }
+
+                                    binding.recyclerView.setAdapter(adapter);
+
+
+                                }
+                            });
+                        }
+                        Log.w(TAG, "Successfully loaded");}
+                });
 
     }
 }
---
+++ app/src/main/java/org/techtown/face/adapters/FamilySettingAdapter.java
@@ -0,0 +1,121 @@
+package org.techtown.face.adapters;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import org.techtown.face.R;
+import org.techtown.face.models.Family;
+import org.techtown.face.models.SearchItem;
+import org.techtown.face.utilites.Constants;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class FamilySettingAdapter extends RecyclerView.Adapter<FamilySettingAdapter.ViewHolder> {
+        ArrayList<Family> items = new ArrayList<>();
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+            View itemView = inflater.inflate(R.layout.search_item, viewGroup, false);
+            return new ViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Family item = items.get(position);
+            holder.setItem(item);
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+
+        static class ViewHolder extends RecyclerView.ViewHolder {
+            TextView nameTxt;
+            TextView mobileTxt;
+            ImageView image;
+            Button add;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+
+                nameTxt = itemView.findViewById(R.id.searchName);
+                mobileTxt = itemView.findViewById(R.id.searchMobile);
+                image = itemView.findViewById(R.id.searchImage);
+                add = itemView.findViewById(R.id.addFamilyButton);
+
+            }
+
+            public void setItem(Family item) {
+                nameTxt.setText(item.getUserContact().name);
+                mobileTxt.setText(item.getUserContact().mobile);
+                StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+                StorageReference imageRef = storageReference.child(item.getUserContact().path);
+                imageRef.getDownloadUrl().addOnSuccessListener(downloadUrl -> Glide.with(itemView)
+                        .load(downloadUrl.toString())
+                        .into(image));
+                FirebaseFirestore database = FirebaseFirestore.getInstance();
+                database.collection(Constants.KEY_COLLECTION_USERS)
+                        .document(item.getUserContact()//여기 잘 처리해라)
+                        .collection(Constants.KEY_COLLECTION_USERS)
+                        .get()
+                        .addOnCompleteListener(task -> {
+                            if(task.isSuccessful()){
+                                for(QueryDocumentSnapshot documentSnapshot : task.getResult()){
+                                    add.setVisibility(View.INVISIBLE);
+                                }
+                            }
+                        });
+
+                add.setOnClickListener(v -> {
+                    add.setVisibility(View.INVISIBLE);
+                    Intent intent = new Intent(itemView.getContext(), //여기 새로 만들어서 채워);
+                    intent.putExtra(Constants.KEY_PATH, item.getPath());
+                    intent.putExtra(Constants.KEY_MOBILE, item.getMobile());
+                    intent.putExtra(Constants.KEY_USER_ID, item.getUserId());
+                    itemView.getContext().startActivity(intent);
+                });
+            }
+        }
+
+
+        public void addItem(Family item) {
+            items.add(item);
+        }
+
+        public void setItems(ArrayList<Family> items) {
+            this.items = items;
+        }
+
+        public Family getItem(int position) {
+            return items.get(position);
+        }
+
+        public void setItem(int position, Family item) {
+            items.set(position, item);
+        }
+
+        public void deleteItem(int position){
+            this.items.remove(position);
+        }
+    }
+}
