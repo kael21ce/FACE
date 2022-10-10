75a7e76d0c7b40176c5ea9f551b2b587e974117f
강지원 <kjason08@kaist.ac.kr>
2022-10-10 21:54:22 +0900

    add FamilyAddActivity and activity_family_add.xml

---
+++ app/src/main/java/org/techtown/face/activities/FamilyAddActivity.java
@@ -0,0 +1,86 @@
+package org.techtown.face.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import org.techtown.face.databinding.ActivityFamilyAddBinding;
+import org.techtown.face.utilites.Constants;
+import org.techtown.face.utilites.PreferenceManager;
+
+import java.util.HashMap;
+
+public class FamilyAddActivity extends AppCompatActivity {
+
+    ActivityFamilyAddBinding binding;
+    PreferenceManager preferenceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFamilyAddBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        Intent intent = getIntent();
+
+        String userId = intent.getStringExtra(Constants.KEY_USER_ID);
+        String path = intent.getStringExtra(Constants.KEY_PATH);
+        String mobile = intent.getStringExtra(Constants.KEY_MOBILE);
+
+        StorageReference reference = FirebaseStorage.getInstance().getReference().child(path);
+        reference.getDownloadUrl().addOnSuccessListener(uri -> Glide.with(this).load(uri).into(binding.image));
+
+        String name = binding.nameTxt.getText().toString();
+        String idealContact = binding.idealContact.getText().toString();
+        String minContact = binding.minContact.getText().toString();
+        String like = binding.themeLike.getText().toString();
+        String dislike = binding.themeDislike.getText().toString();
+
+
+        binding.add.setOnClickListener(v -> {
+            long now = System.currentTimeMillis();
+            HashMap<String,Object> user = new HashMap<>();
+            user.put(Constants.KEY_USER, userId);
+            user.put(Constants.KEY_WINDOW, now);
+            user.put(Constants.KEY_EXPRESSION, 5);
+            user.put(Constants.KEY_NAME, name);
+            user.put(Constants.KEY_MOBILE, mobile);
+            user.put(Constants.KEY_IDEAL_CONTACT, "");
+            user.put(Constants.KEY_MIN_CONTACT, "");
+            user.put(Constants.KEY_THEME_LIKE, "");
+            user.put(Constants.KEY_THEME_DISLIKE, "");
+
+            HashMap<String, Object> myUser = new HashMap<>();
+            myUser.put(Constants.KEY_USER, preferenceManager.getString(Constants.KEY_USER_ID));
+            myUser.put(Constants.KEY_WINDOW, now);
+            myUser.put(Constants.KEY_EXPRESSION, 5);
+            myUser.put(Constants.KEY_NAME, preferenceManager.getString(Constants.KEY_NAME));
+            myUser.put(Constants.KEY_MOBILE, preferenceManager.getString(Constants.KEY_MOBILE));
+            myUser.put(Constants.KEY_IDEAL_CONTACT, idealContact);
+            myUser.put(Constants.KEY_MIN_CONTACT, minContact);
+            myUser.put(Constants.KEY_THEME_LIKE, like);
+            myUser.put(Constants.KEY_THEME_DISLIKE, dislike);
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            db.collection(Constants.KEY_COLLECTION_USERS)
+                    .document(preferenceManager.getString(Constants.KEY_USER_ID))
+                    .collection(Constants.KEY_COLLECTION_USERS)
+                    .add(user);
+
+            db.collection(Constants.KEY_COLLECTION_USERS)
+                    .document(userId)
+                    .collection(Constants.KEY_COLLECTION_USERS)
+                    .add(myUser);
+
+            Intent intent1 = new Intent(FamilyAddActivity.this, MainActivity.class);
+            startActivity(intent1);
+            finish();
+        });
+    }
+}
