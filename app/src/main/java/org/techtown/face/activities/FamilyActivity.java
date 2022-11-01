caff523fd5abe92857f25802ac4784ccfdbcc6d3
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-11-01 14:46:26 +0900

    Change UI of FamilyActivity

--- app/src/main/java/org/techtown/face/activities/FamilyActivity.java
+++ app/src/main/java/org/techtown/face/activities/FamilyActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 
 import org.techtown.face.databinding.ActivityFamilyBinding;
+import org.techtown.face.fragments.MomentFragment;
 import org.techtown.face.models.User;
 import org.techtown.face.utilites.Constants;
 import org.techtown.face.utilites.PreferenceManager;
@@ -36,12 +38,15 @@ public class FamilyActivity extends BaseActivity {
 
         binding.exit.setOnClickListener(v -> finish());
         binding.familyName.setText(user.name);
-        binding.minContact.setText("적어도 " + user.min_contact + "일에 1번");
-        binding.idealContact.setText(user.ideal_contact + "일에 1번");
+        binding.hope.setText(user.name + "의 마음을 전합니다");
+        binding.minContact.setText("아무리 적어도 " + user.min_contact + "에 1번씩은 연락하면 좋겠어...");
+        binding.idealContact.setText(user.ideal_contact + "일에 1번씩 연락하면 정말 좋을 것 같아!");
         binding.callButton.setOnClickListener(v -> {
             Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + user.mobile));
             startActivity(callIntent);
         });
+        binding.momentButton.setText(user.name + "의 순간 보러 가기");
+        //순간으로 이동하는 것 추가하기
         binding.chatButton.setOnClickListener(view -> {
             Intent intent1 = new Intent(getApplicationContext(), ChatActivity.class);
             intent1.putExtra(Constants.KEY_USER, user);
@@ -49,8 +54,8 @@ public class FamilyActivity extends BaseActivity {
             finish();
         });
         binding.contactButton.setOnClickListener(v -> sendContact());
-        binding.themeLike.setText(user.like);
-        binding.themeDislike.setText(user.dislike);
+        binding.themeLike.setText(user.like + " 이야기를 함께한다면 정말 즐거울 것 같아!");
+        binding.themeDislike.setText(user.dislike + " 이야기는 조금 줄여주면 좋겠어...");
     }
 
     private void sendContact(){
