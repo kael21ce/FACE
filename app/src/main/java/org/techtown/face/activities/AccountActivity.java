63ca27677ae763dcfdab10cc6a7adbca661a4b04
강지원 <kjason08@kaist.ac.kr>
2022-11-10 15:56:51 +0900

    Change setting UI into better one in AccountActivity and activity_account

--- app/src/main/java/org/techtown/face/activities/AccountActivity.java
+++ app/src/main/java/org/techtown/face/activities/AccountActivity.java
@@ -44,9 +44,7 @@ public class AccountActivity extends BaseActivity {
         binding.changeInfo.setOnClickListener(view -> changeInformation());
         binding.phoneNumber.setText(preferenceManager.getString(Constants.KEY_MOBILE));
         binding.imageView.setImageBitmap(getUserImage(preferenceManager.getString(Constants.KEY_IMAGE)));
-        binding.like.setText(preferenceManager.getString(Constants.KEY_THEME_LIKE));
-        binding.dislike.setText(preferenceManager.getString(Constants.KEY_THEME_DISLIKE));
-
+        
     }
 
     private void changeInformation(){
